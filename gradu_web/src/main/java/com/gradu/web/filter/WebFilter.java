d9ff27747046e9364ddca0c9b4eaff71b403f057
wuyiliang <tuibii@163.com>
2019-10-17 17:51:32 +0800

    zuul

---
+++ gradu_web/src/main/java/com/gradu/web/filter/WebFilter.java
@@ -0,0 +1,30 @@
+package com.gradu.web.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("!!!!!!!!!!!!!!!!");
+        return null;
+    }
+}
