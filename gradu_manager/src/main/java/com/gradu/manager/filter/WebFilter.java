27a36d3ba02f08efaa6cbd42b924f72d9a741def
wuyiliang <tuibii@163.com>
2019-10-18 17:23:07 +0800

    后台路由校验权限

--- gradu_manager/src/main/java/com/gradu/manager/filter/WebFilter.java
+++ gradu_manager/src/main/java/com/gradu/manager/filter/WebFilter.java
@@ -1,11 +1,21 @@
 package com.gradu.manager.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import util.JwtUtil;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class WebFilter extends ZuulFilter {
+
+    @Autowired
+    JwtUtil jwtUtil;
 
     @Override
     public String filterType() {
@@ -24,7 +34,41 @@ public class WebFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
-        System.out.println("!!!!!!!!!!!!!!!!");
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String token = request.getHeader("token");
+
+        if (request.getMethod().equals("")){
+            return null;
+        }
+
+        String url = request.getRequestURL().toString();
+        if (url.indexOf("/admin/login")>0){
+            return null;
+        }
+
+        if (!StringUtils.isEmpty(token)){
+            try{
+                Claims claims = jwtUtil.parseToken(token);
+                String role = (String) claims.get("role");
+
+                if (role.equals("admin")){
+                    currentContext.addZuulRequestHeader("token",token);
+                    return null;
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+                currentContext.setSendZuulResponse(false);
+                currentContext.setResponseStatusCode(401);
+                currentContext.setResponseBody("无权限");
+            }
+        }
+
+
+        currentContext.setSendZuulResponse(false);
+        currentContext.setResponseStatusCode(401);
+        currentContext.setResponseBody("无权限");
         return null;
     }
 }
