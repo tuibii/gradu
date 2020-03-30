3e3f48eeda75ae2c19734fda4896d5371d844352
wuyiliang <tuibii@163.com>
2020-03-31 01:28:36 +0800

    OSS跨域处理

--- gradu_gathering/src/main/java/com/gradu/gathering/controller/OssController.java
+++ gradu_gathering/src/main/java/com/gradu/gathering/controller/OssController.java
@@ -4,13 +4,11 @@ import com.gradu.gathering.service.OssService;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin
 @RequestMapping("oss")
 public class OssController {
 
